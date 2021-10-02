29cfedebc2e8dc56605a905ae386ad6dcd83b0e7
kin-lou <85361562+kin-lou@users.noreply.github.com>
2021-10-02 15:21:43 +0800

    Add files via upload

--- setting.py
+++ setting.py
@@ -1,14 +1,17 @@
 import os
 env_path = './.env'
 
-with open(env_path, 'r') as f:
-    for line in f.readlines():
-        # 跳出空白行
-        if not line.strip():
-            continue
+try:
+    with open(env_path, 'r') as f:
+        for line in f.readlines():
+            # 跳出空白行
+            if not line.strip():
+                continue
 
-        line = line.replace('\n', '')
-        if not line.startswith('#'):
-            key = line.split('=', 1)[0]
-            val = line.split('=', 1)[1]
-            os.environ[key] = val
+            line = line.replace('\n', '')
+            if not line.startswith('#'):
+                key = line.split('=', 1)[0]
+                val = line.split('=', 1)[1]
+                os.environ[key] = val
+except:
+    print('didn\'t have .env file')
