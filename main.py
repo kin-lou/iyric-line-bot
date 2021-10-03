12bb41691312b246076179a7d5556fd59ae34817
kin-lou <85361562+kin-lou@users.noreply.github.com>
2021-10-03 15:23:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -102,6 +102,8 @@ def handle_message(event):
 
     # line_bot_api.reply_message(event.reply_token, message)
         line_bot_api.reply_message(event.reply_token, test_button)
+    elif message == 'test=postback_台中市':
+        pass
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
