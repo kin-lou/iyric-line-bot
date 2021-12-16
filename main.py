6bda62ec515bc8fdd3ea0acf2afcedeca099e96f
kin-lou <85361562+kin-lou@users.noreply.github.com>
2021-12-17 00:19:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import datetime
 
@@ -8,9 +9,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
-import urllib
-import requests
-from bs4 import BeautifulSoup
+import twstock
 
 import setting
 
@@ -21,134 +20,113 @@ line_bot_api = LineBotApi(os.environ['LINE_BOT_TOKEN'])
 # Channel Secret
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 # Developer ID
-line_bot_api.push_message(
-    os.environ['DEV_UID'], TextSendMessage(text='start cmd'))
-
+line_bot_api.push_message(os.environ['DEV_UID'], TextSendMessage(text='start cmd'))
 
 def crawl_by_cd(url):
     all_item = []
     try:
         _ = '待規劃'
-        # resp = requests.get(url)
-        # soup = BeautifulSoup(resp.text, 'html.parser')
-        # items = soup.select('table.iB > tr > td > div > dl > dd')[1:100]
 
-        # for i in items:
-        #     if i.select('p'):
-        #         continue
-
-        #     text = ''
-        #     for k in i.select('a'):
-        #         text += k.text + '\n'
-
-        #     all_item.append({
-        #         'text': text,
-        #         'sub_url': i.select('a')[-1].get('href')
-        #     })
     except:
         pass
     return all_item
 
+# def crawl_by_song(url):
+#     all_item = []
+#     try:
+#         resp = requests.get(url)
+#         soup = BeautifulSoup(resp.text, 'html.parser')
+#         items = soup.select('table.iB > tr > td > div > dl > dd')[1:100]
 
-def crawl_by_song(url):
-    all_item = []
-    try:
-        resp = requests.get(url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        items = soup.select('table.iB > tr > td > div > dl > dd')[1:100]
+#         for i in items:
+#             if i.select('p'):
+#                 continue
 
-        for i in items:
-            if i.select('p'):
-                continue
+#             text = ''
+#             for k in i.select('a'):
+#                 text += k.text + '\n'
 
-            text = ''
-            for k in i.select('a'):
-                text += k.text + '\n'
+#             all_item.append({
+#                 'text': text,
+#                 'sub_url': i.select('a')[-1].get('href')
+#             })
+#     except:
+#         pass
+#     return all_item
 
-            all_item.append({
-                'text': text,
-                'sub_url': i.select('a')[-1].get('href')
-            })
-    except:
-        pass
-    return all_item
+# def crawl_by_url(url):
+#     search_list = []
+#     try:
+#         mode = url.split('?')[-1]
+#         if mode == 't2':
+#             search_list = crawl_by_cd(url)
+#         elif mode == 't3':
+#             search_list = crawl_by_song(url)
 
+#         if len(search_list) == 0:
+#             raise Exception
 
-def crawl_by_url(url):
-    search_list = []
-    try:
-        mode = url.split('?')[-1]
-        if mode == 't2':
-            search_list = crawl_by_cd(url)
-        elif mode == 't3':
-            search_list = crawl_by_song(url)
+#         cnt_columns = 0
+#         flag = False
+#         columns = []
+#         actions = []
+#         for item in search_list:
+#             flag = False
+#             actions.append(
+#                 URIAction(
+#                     label=item['text'][:20],
+#                     uri=f'https://mojim.com{item["sub_url"]}'
+#                 )
+#             )
 
-        if len(search_list) == 0:
-            raise Exception
+#             if len(columns) == cnt_columns:
+#                 columns.append('')
+#             columns[cnt_columns] = CarouselColumn(
+#                 text=f'分頁_{cnt_columns + 1}',
+#                 actions=actions
+#             )
 
-        cnt_columns = 0
-        flag = False
-        columns = []
-        actions = []
-        for item in search_list:
-            flag = False
-            actions.append(
-                URIAction(
-                    label=item['text'][:20],
-                    uri=f'https://mojim.com{item["sub_url"]}'
-                )
-            )
+#             if len(actions) % 3 == 0:
+#                 cnt_columns += 1
+#                 flag = True
+#                 actions = []
 
-            if len(columns) == cnt_columns:
-                columns.append('')
-            columns[cnt_columns] = CarouselColumn(
-                text=f'分頁_{cnt_columns + 1}',
-                actions=actions
-            )
+#         if flag is False:
+#             columns.pop()
 
-            if len(actions) % 3 == 0:
-                cnt_columns += 1
-                flag = True
-                actions = []
+#         template = CarouselTemplate(columns=columns[:10])
+#         return True, template
+#     except Exception as e:
+#         return False, 'Sorry, you get some error'
 
-        if flag is False:
-            columns.pop()
+# def get_crawl_mode_button(song_name):
+#     song_name = urllib.parse.quote(song_name.encode('utf8'))
+#     actions = []
+#     all_mode = ['專輯', '歌名']
+#     for mode in all_mode:
+#         actions.append(
+#             PostbackTemplateAction(
+#                 label=mode,
+#                 text=mode,
+#                 data=f'https://mojim.com/{song_name}.html?t{all_mode.index(mode) + 2}'
+#             )
+#         )
 
-        template = CarouselTemplate(columns=columns[:10])
-        return True, template
-    except Exception as e:
-        return False, 'Sorry, you get some error'
+#     actions.append(
+#         URIAction(
+#             label='test',
+#             uri=f'https://iyric-line-bot.herokuapp.com/test'
+#         )
+#     )
 
-
-def get_crawl_mode_button(song_name):
-    song_name = urllib.parse.quote(song_name.encode('utf8'))
-    actions = []
-    all_mode = ['專輯', '歌名']
-    for mode in all_mode:
-        actions.append(
-            PostbackTemplateAction(
-                label=mode,
-                text=mode,
-                data=f'https://mojim.com/{song_name}.html?t{all_mode.index(mode) + 2}'
-            )
-        )
-
-    actions.append(
-        URIAction(
-            label='test',
-            uri=f'https://iyric-line-bot.herokuapp.com/test'
-        )
-    )
-
-    template = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-            text='請選擇搜尋條件',
-            actions=actions
-        )
-    )
-    return template
-
+#     template = TemplateSendMessage(
+#         alt_text='Buttons template',
+#         template=ButtonsTemplate(
+#             text='請選擇搜尋條件',
+#             actions=actions
+#         )
+#     )
+#     return template
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -160,12 +138,10 @@ def test():
     }
     return jsonify(data)
 
-
 @app.route('/callback', methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    print('============\n', signature, len(signature), '\n============')
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -179,29 +155,29 @@ def callback():
     return 'OK'
 
 # 訊息傳遞區塊
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
+    m = re.search('([0-9]*)', message)
+    print(m.group())
+    stock_code = m.group(1)
+    print(stock_code)
     if message in ['專輯', '歌名']:
         pass
     else:
-        line_bot_api.reply_message(
-            event.reply_token, get_crawl_mode_button(message))
-
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(stock_code))
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
     url = event.postback.data
-    status, data = crawl_by_url(url)
+    # status, data = crawl_by_url(url)
+    status, data = False, 'test'
 
     if status:
         line_bot_api.reply_message(
             event.reply_token, TemplateSendMessage(alt_text='結果', template=data))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(data))
-
 
 # 主程式
 if __name__ == '__main__':
